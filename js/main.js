0a00d63e1bf1ed9e019e7c2cba55632c619f1f58
Nallely House <nallelyht@gmail.com>
2017-05-29 14:18:59 -0500

    maquetacion

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//jQuery(document).ready(function (){
+//	alert("Hola!")
+//});
+
+/*function saludar(){
+document.getElementById("saludo).innerHTML="saludo";
+}*/
+
+//$(document).ready(function(){
+//	$("button").click(function(){
+//		$("#saludo").append("Hola!")
+//	});
+//});
+
+//$(document).ready(function () {
+//	$("#cambiar-color").click(function () {
+//		$("#resaltar").css("color", "red");
+//	});
+//	$("button").click(function () {
+//		$("ul").append("<li>Mas comida que Jquery</li>");
+//	});
+//});
+$(document).ready(function (){
+	
+})
